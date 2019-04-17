c5048552021d0a88481ff6dd2d21b8d0f1c28030
YeFan <yefan1@lenovo.com>
2019-04-17 15:56:05 +0800

    update

---
+++ article/1.js
@@ -0,0 +1,40 @@
+curl -X GET -H "User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Mobile/14E277 MicroMessenger/6.6.3 NetType/WIFI Language/zh_CN" -H "Cookie: code=Z5TpOf; uid=22532318" -H "Host: wx.yysports.com" -H "Referer: http://wx.yysports.com/limitelottery/product-list.html" "http://wx.yysports.com/limitelottery/success.html"
+
+curl -X GET -H "User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Mobile/14E277 MicroMessenger/6.6.3 NetType/WIFI Language/zh_CN" -H "X-Requested-With: XMLHttpRequest" -H "Host: wx.yysports.com" -H "Referer: http://wx.yysports.com/limitelottery/form.html?code=Z5TpOf&state=a77b716808924358b88053089d6b660e" "http://wx.yysports.com/limitelottery/regist/checkssologin?code=Z5TpOf&redirecturl=form.html"
+
+curl -k -X GET -H "User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Mobile/14E277 MicroMessenger/6.6.3 NetType/WIFI Language/zh_CN" -H "Cookie: tssoid=e50f1a2f-bf6b-4a6b-b503-60f378533c39" -H "Host: sso-prod.yysports.com" -H "Referer: https://sso-prod.yysports.com/login?redirect_uri=http://wx.yysports.com/limitelottery/form.html&from=200000211&fromType=1&client_id=matrix&state=a77b716808924358b88053089d6b660e" "https://sso-prod.yysports.com/oauth/authorize?client_id=matrix&redirect_uri=http%3A%2F%2Fwx.yysports.com%2Flimitelottery%2Fform.html&response_type=code&state=a77b716808924358b88053089d6b660e"
+
+curl -X GET -H "User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Mobile/14E277 MicroMessenger/6.6.3 NetType/WIFI Language/zh_CN" -H "Cookie: code=Z5TpOf; uid=22532318" -H "X-Requested-With: XMLHttpRequest" -H "Host: wx.yysports.com" -H "Referer: http://wx.yysports.com/limitelottery/product-list.html" -H "Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJsaW1pdGVkX2VkaXRpb25fbG90dGVyeSIsImlhdCI6MTU1NDk0ODkwMiwianRpIjoiMjI1MzIzMTgiLCJzdWIiOiIxMzAzNzE2ODU4MCIsImxpbWl0ZWRVc2VyIjp0cnVlfQ.N37RbY3JoMNo9166B8ugrxOAFfA-xhe1cfSL4suEMrw" "http://wx.yysports.com/limitelottery/activity"
+
+curl -X POST -H "Cookie: code=Z5TpOf; uid=22532318" -H "Origin: http://wx.yysports.com" -H "Host: wx.yysports.com" -H "User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Mobile/14E277 MicroMessenger/6.6.3 NetType/WIFI Language/zh_CN" -H "Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJsaW1pdGVkX2VkaXRpb25fbG90dGVyeSIsImlhdCI6MTU1NDk0ODkwMiwianRpIjoiMjI1MzIzMTgiLCJzdWIiOiIxMzAzNzE2ODU4MCIsImxpbWl0ZWRVc2VyIjp0cnVlfQ.N37RbY3JoMNo9166B8ugrxOAFfA-xhe1cfSL4suEMrw" -H "Referer: http://wx.yysports.com/limitelottery/product-list.html" -H "X-Requested-With: XMLHttpRequest" -d "[{\"activityItemId\":\"843\",\"activityShopId\":\"2705\"},{\"activityItemId\":\"844\",\"activityShopId\":\"2720\"}]" "http://wx.yysports.com/limitelottery/activity"
+
+curl -X GET -H "User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Mobile/14E277 MicroMessenger/6.6.3 NetType/WIFI Language/zh_CN" -H "Host: wx.yysports.com" "http://wx.yysports.com/limitelottery/form.html?code=Z5TpOf&state=a77b716808924358b88053089d6b660e"
+
+curl -X GET -H "User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Mobile/14E277 MicroMessenger/6.6.3 NetType/WIFI Language/zh_CN" -H "Cookie: code=Z5TpOf; uid=22532318" -H "Host: wx.yysports.com" -H "Referer: http://wx.yysports.com/limitelottery/success.html" "http://wx.yysports.com/limitelottery/images/jordan/success-banner.jpg"
+
+curl -X GET -H "User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Mobile/14E277 MicroMessenger/6.6.3 NetType/WIFI Language/zh_CN" -H "Cookie: code=Z5TpOf; uid=22532318" -H "Host: wx.yysports.com" -H "Referer: http://wx.yysports.com/limitelottery/form.html?code=Z5TpOf&state=a77b716808924358b88053089d6b660e" "http://wx.yysports.com/limitelottery/product-list.html"
+
+curl -k -X POST -H "User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Mobile/14E277 MicroMessenger/6.6.3 NetType/WIFI Language/zh_CN" -H "x-requested-with: XMLHttpRequest" -H "Host: sso-prod.yysports.com" -H "Referer: https://sso-prod.yysports.com/login?redirect_uri=http://wx.yysports.com/limitelottery/form.html&from=200000211&fromType=1&client_id=matrix&state=a77b716808924358b88053089d6b660e" -H "Origin: https://sso-prod.yysports.com" -d "{\"username\":\"13037168580\",\"password\":\"Yf458232\",\"client_id\":\"matrix\",\"redirect_uri\":\"http://wx.yysports.com/limitelottery/form.html\",\"response_type\":\"code\",\"state\":\"a77b716808924358b88053089d6b660e\"}" "https://sso-prod.yysports.com/api/member/pousheng/account/login"
+
+
+
+curl -X POST -H "Cache-Control: no-cache" -H "Host: szshort.weixin.qq.com" -H "User-Agent: MicroMessenger Client" --data-binary '@req_raw_67a29b8fafd57925.thor_bak' "http://szshort.weixin.qq.com/cgi-bin/micromsg-bin/3rd-geta8key"
+
+
+http://wx.yysports.com/limitelottery/success.html
+
+http://wx.yysports.com/limitelottery/regist/checkssologin?code=Z5TpOf&redirecturl=form.html
+
+https://sso-prod.yysports.com/oauth/authorize?client_id=matrix&redirect_uri=http%3A%2F%2Fwx.yysports.com%2Flimitelottery%2Fform.html&response_type=code&state=a77b716808924358b88053089d6b660e
+
+http://wx.yysports.com/limitelottery/activity
+
+http://wx.yysports.com/limitelottery/activity
+
+http://wx.yysports.com/limitelottery/form.html?code=Z5TpOf&state=a77b716808924358b88053089d6b660e
+
+https://sso-prod.yysports.com/api/member/pousheng/account/login
+
+http://wx.yysports.com/limitelottery/product-list.html
+
+http://szshort.weixin.qq.com/cgi-bin/micromsg-bin/3rd-geta8key
--- article/工程化工具学习/webpack/webpack.md
+++ article/工程化工具学习/webpack/webpack.md
@@ -3,16 +3,52 @@
 2. 安装webpack和webpack-cli
 3. webpack配置项 创建webpack.config.js(默认，可修改)文件来配置webpack
 4. 具体插件配置查看文档
+>es6Js文件热更新使用babel
+CSS文件使用
 ```javascript
 // 基于node
+var htmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
     entry: '',               // 入口文件
     output: {},              // 出口文件
     module: {                // 处理对应模块
-        rules: []
+        rules: [
+                    {
+                        test: /\.js$/,
+                        exclude: /node_modules/,
+                        loader: 'babel-loader'
+                    }
+                ]
     },              
-    plugins: [],             // 对应的插件
+    plugins: [               // 对应的插件
+        // html 生成插件
+        new htmlWebpackPlugin()
+    ],             
     devServer: {},           // 开发服务器配置
     mode: 'development'      // 模式配置
 }
+```
+
+5. 热更新需要配置本地服务器webpack-dev-server及html-webpack-plugin
+>webpack.config.js里配置， npm安装插件
+
+```javascript
+
+plugins: [               // 对应的插件
+        // html 生成插件
+        new htmlWebpackPlugin()
+    ],
+devServer: {    // 开发服务器配置
+        contentBase: path.join(__dirname, "dist")
+    }
+```
+
+6. npm简化启动命令
+在package.json中设置script的命令
+```javascript
+"scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1",
+    "dev": "webpack-dev-server --open"
+  }
 ```
